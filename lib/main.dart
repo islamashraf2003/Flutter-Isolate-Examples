6bcf589ad6a86f1d92b107b920bc4295008c0959
islamashraf2003 <islamashraf520@gmail.com>
2025-11-05 00:56:41 +0200

    feat: implement isolate example for heavy task processing

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,84 @@
+import 'dart:isolate';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: IsolateDemo()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Isolate Examples',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-      home: const IsolateDemo(),
-    );
-  }
-}
-
-class IsolateDemo extends StatelessWidget {
+class IsolateDemo extends StatefulWidget {
   const IsolateDemo({super.key});
 
   @override
+  State<IsolateDemo> createState() => _IsolateDemoState();
+}
+
+/// This function is placed outside the widget tree because it runs in a separate isolate.
+/// Isolates cannot access variables, state, or context from the main isolate.
+/// It only communicates with the main isolate using the SendPort.
+void heavyTask(SendPort sendPort) {
+  int sum = 0;
+  for (int i = 0; i < 500000000; i++) {
+    sum += i;
+  }
+
+  sendPort.send(sum);
+}
+
+class _IsolateDemoState extends State<IsolateDemo> {
+  String result = 'No result right now';
+
+  Future<void> runHeavyTaskWithIsolate() async {
+    setState(() {
+      result = 'Calculating (with isolate)...';
+    });
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(heavyTask, receivePort.sendPort);
+    dynamic sum = await receivePort.first;
+
+    setState(() {
+      result = 'Result (with isolate) = $sum';
+    });
+  }
+
+  Future<void> runHeavyTaskWithoutIsolate() async {
+    setState(() {
+      result = 'Calculating (without isolate)...';
+    });
+
+    int sum = 0;
+    for (int i = 0; i < 500000000; i++) {
+      sum += i;
+    }
+
+    setState(() {
+      result = 'Result (no isolate) = $sum';
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column());
+    return Scaffold(
+      appBar: AppBar(title: const Text('Isolate Demo')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(result, textAlign: TextAlign.center),
+            const SizedBox(height: 20),
+            CircularProgressIndicator(color: Colors.black),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: runHeavyTaskWithIsolate,
+              child: const Text('Heavy Task'),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: runHeavyTaskWithoutIsolate,
+              child: const Text('Run Heavy Task WITHOUT Isolate'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
